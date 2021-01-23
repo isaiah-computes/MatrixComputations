ab9154da930d78110988ec36430479367cc37698
id <id@kl>
2021-01-22 16:53:50 -0900

    Add operator demo to main

--- main.cpp
+++ main.cpp
@@ -7,12 +7,22 @@ using std::cout;
 int main()
 {
     // quick test of element assignment
-    Matrix m = Matrix::Matrix(3, 3);
+    Matrix m(3, 3);
     m(0, 0) = 1;
     m(1, 1) = 2;
     m(2, 2) = 3;
 
     m.Print();
+
+    Matrix identity = Matrix::Identity(3);
+
+    Matrix mPlus = identity + m;
+    cout << "\n";
+    mPlus.Print();
+
+    Matrix mMinus = identity - m;
+    cout << "\n";
+    mMinus.Print();
 
     std::cin.get();
     return 0;
