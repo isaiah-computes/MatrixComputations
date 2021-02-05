6fa664e599f490c4fbe154f8fb0f8fa79bc8c9e2
isaiah <iditmer@alaska.edu>
2021-02-05 13:49:51 -0900

    Implement testing of +/- exceptions

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,26 +2,32 @@
 #include "../src/Matrix.hpp"
 
 
+// interesting : catch2 supports vector comparisons
+/*for (int i = 0; i < num_rows; i++){
+    REQUIRE_THAT(m[i], Catch::Matchers::Equals(t[i]));
+}*/
+
+TEST_CASE("Addition and Subtraction Require Same Dimensions")
+{
+    Matrix m1 = Matrix::Random(5, 7);
+    Matrix m2 = Matrix::Random(7, 5);
+
+    REQUIRE_THROWS_AS(m1 + m2, std::invalid_argument);
+    REQUIRE_THROWS_AS(m1 - m2, std::invalid_argument);
+}
+
 TEST_CASE( "Identity generates diagonal matrix of ones") {
 
-    int num_rows = 5;
+    int size = 5;
 
-    Matrix m = Matrix::Identity(num_rows);
-    Matrix t = Matrix(num_rows,num_rows);
+    Matrix m = Matrix::Identity(size);
 
-    for (int i = 0; i < num_rows; i++) {
-        t(i,i) = 1.0;
-    }
-
-    for (int i = 0; i<num_rows; i++)
+    for (int i = 0; i<size; i++)
     {
-        for (int j = 0; j < num_rows; j++)
+        for (int j = 0; j < size; j++)
         {
-            REQUIRE(m(i,j)==t(i,j));
+            if (i == j) REQUIRE(m(i, j) == 1.0);
+            else REQUIRE(m(i, j) == 0.0);
         }
     }
-
-    /*for (int i = 0; i < num_rows; i++){
-        REQUIRE_THAT(m[i], Catch::Matchers::Equals(t[i]));
-    }*/
 }
