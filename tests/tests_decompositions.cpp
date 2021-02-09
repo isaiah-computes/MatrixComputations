85ff7f2961a53fde3547f2f11d10088289cd3383
id-uaa <iditmer@alaska.edu>
2021-02-08 23:29:18 -0900

    Implement LU correctness test

--- tests/tests_decompositions.cpp
+++ tests/tests_decompositions.cpp
@@ -10,3 +10,27 @@ TEST_CASE("LU Decomoposiiton requires square input") {
 
 	REQUIRE_THROWS_AS(MatrixDecompositions::LU(A, L, U), std::invalid_argument);
 }
+
+TEST_CASE("LU Decomoposiiton is correct") {
+
+	int size = 5;
+	Matrix A = Matrix::Random(size, size);
+	Matrix L(size, size); Matrix U(size, size);
+
+	MatrixDecompositions::LU(A, L, U);
+
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = i + 1; j < size; j++) {
+			REQUIRE(L(i, j) == 0);
+		}
+	}
+
+	for (int i = 1; i < size; i++) {
+		for (int j = 0; j < i; j++) {
+			REQUIRE(U(i, j) == 0);
+		}
+	}
+
+	bool equivalance = A == (L * U);
+	REQUIRE(equivalance == true);
+}
