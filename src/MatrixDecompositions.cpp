ce8dd4f1bbf4e0b501139f5761a44515de5e82ce
id-uaa <iditmer@alaska.edu>
2021-02-08 23:38:14 -0900

    Implement LU Decomposition

--- src/MatrixDecompositions.cpp
+++ src/MatrixDecompositions.cpp
@@ -1,15 +1,36 @@
 #include "Matrix.hpp"
 #include "MatrixDecompositions.hpp"
 
-
 void MatrixDecompositions::LU(const Matrix A, Matrix& L, Matrix& U)
 {
-	if (A.Rows() != A.Columns()) throw std::invalid_argument("LU Decomposition requires square input");
+    int n = A.Rows();
+	if (n != A.Columns()) throw std::invalid_argument("LU Decomposition requires square input");
 
 	// ALGORITHM 3.2.1 FROM MATRIX COMPUTATIONS, 4TH ED
+	
+    L = Matrix::Identity(n);
+	U = Matrix(A);
 
-	//U = Matrix(A);
+    for (int k = 0; k < n - 1; k++)
+    {
+        for (int i = k + 1; i < n; i++) U(i, k) /= U(k, k);
 
+        for (int i = k + 1; i < n; i++)
+        {
+            for (int j = k + 1; j < n; j++)
+            {
+                U(i, j) -= U(i, k) * U(k, j);
+            }
+        }
+    }
 
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            L(i, j) = U(i, j);
+            U(i, j) = 0;
+        }
+    }
 }
 
