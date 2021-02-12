b1924a983ae9a049d99df671f2002788ca2a1908
isaiah <iditmer@alaska.edu>
2021-02-12 11:34:08 -0900

    Implement basis vector ctor test

--- tests/test_vectors.cpp
+++ tests/test_vectors.cpp
@@ -8,3 +8,15 @@ TEST_CASE("Vector ctor generates appropriate length") {
 
 	REQUIRE(v.Length() == size);
 }
+
+TEST_CASE("Basis vector ctor functions") {
+	int dim = 17;
+	int basis = 5;
+
+	RealVector v = RealVector::StandardBasis(dim, basis);
+
+	for (int i = 0; i < dim; i++) {
+		if (i != basis) REQUIRE(v[i] == 0);
+		else REQUIRE(v[i] == 1.0);
+	}
+}
