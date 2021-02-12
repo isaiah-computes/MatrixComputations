b84bccf3ba05755ce6235806b25d65e404662515
isaiah <iditmer@alaska.edu>
2021-02-12 11:34:22 -0900

    Implement basis vector static ctor

--- src/RealVector.cpp
+++ src/RealVector.cpp
@@ -1,6 +1,26 @@
+#include <stdexcept>
 #include "RealVector.hpp"
 
 RealVector::RealVector(unsigned int size)
 {
 	entries.resize(size);
+}
+
+double& RealVector::operator[](const unsigned int index)
+{
+	if (index >= entries.size()) throw std::out_of_range("Invalid index value.");
+	return this->entries[index];
+}
+
+const double& RealVector::operator[](const unsigned int index) const
+{
+	if (index >= entries.size()) throw std::out_of_range("Invalid index value.");
+	return this->entries[index];
+}
+
+RealVector RealVector::StandardBasis(unsigned int number_dimensions, unsigned int basis_dimension)
+{
+	RealVector v(number_dimensions);
+	v[basis_dimension] = 1.0;
+	return v;
 }
--- src/RealVector.hpp
+++ src/RealVector.hpp
@@ -13,6 +13,11 @@ public:
 	
 	RealVector(unsigned int size);
 	unsigned int Length() { return entries.size(); }
+
+	double& operator[](const unsigned int index);
+	const double& operator[](const unsigned int index) const;
+
+	static RealVector StandardBasis(unsigned int number_dimensions, unsigned int basis_dimension);
 };
 
 #endif
