cf6cbec6491a722ba3189a1e42244bccd528029b
isaiah <iditmer@alaska.edu>
2021-04-12 14:35:37 -0800

    Test initilizer list matrix ctor

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -17,6 +17,34 @@ TEST_CASE("Constructor generates correct dimensions") {
     REQUIRE(m.Columns() == num_cols);
 }
 
+TEST_CASE("Initializer list ctor functions properly") {
+
+    NumericVector v1{ 15,16,17 };
+    NumericVector v2{ 22,23,24 };
+    NumericVector v3{ 19,15,11 };
+    NumericVector v4{ 37,30,23 };
+    Matrix m{ v1,v2,v3,v4 };
+
+    REQUIRE(m.Rows() == 4);
+    REQUIRE(m.Columns() == 3);
+
+    REQUIRE(m(0, 0) == 15);
+    REQUIRE(m(0, 1) == 16);
+    REQUIRE(m(0, 2) == 17);
+
+    REQUIRE(m(1, 0) == 22);
+    REQUIRE(m(1, 1) == 23);
+    REQUIRE(m(1, 2) == 24);
+
+    REQUIRE(m(2, 0) == 19);
+    REQUIRE(m(2, 1) == 15);
+    REQUIRE(m(2, 2) == 11);
+
+    REQUIRE(m(3, 0) == 37);
+    REQUIRE(m(3, 1) == 30);
+    REQUIRE(m(3, 2) == 23);
+}
+
 TEST_CASE("Indexing by row and column works correctly") {
 
     int num_rows = 4;
