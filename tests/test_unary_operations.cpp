6174cbcffee0fac7246962f122202b2da7c51b9e
isaiah <iditmer@alaska.edu>
2021-04-16 12:23:49 -0800

    Add line to matrix read/write test to delete temp file

--- tests/test_unary_operations.cpp
+++ tests/test_unary_operations.cpp
@@ -1,37 +1,40 @@
+#include <cstdio>
+#include <string>
 #include "catch.hpp"
 #include "Matrix.hpp"
 
-TEST_CASE("Writing and reading matrices maintain dimensions") {
+TEST_CASE("Writing and reading matrices maintain values", "[unary-ops][io]") {
+
+    std::string fname = "test_matrix.txt";
 
     Matrix m = Matrix::Random(7, 12);
-    m.ToFile("test_mat.txt");
-    Matrix m2 = Matrix::FromFile("test_mat.txt");
+    m.ToFile(fname);
+    Matrix m2 = Matrix::FromFile(fname);
+    bool matching_matrices = m == m2;
 
     REQUIRE(m.Rows() == m2.Rows());
     REQUIRE(m.Columns() == m2.Columns());
-}
-
-TEST_CASE("Writing and reading matrices maintain values") {
-
-    Matrix m = Matrix::Random(7, 12);
-    m.ToFile("test_mat.txt");
-    Matrix m2 = Matrix::FromFile("test_mat.txt");
-    bool matching_matrices = m == m2;
-
     REQUIRE(matching_matrices == true);
+
+    std::remove(fname.c_str());
 }
 
-TEST_CASE("Matrix transpose produces correct output") {
+TEST_CASE("Matrix transpose produces correct output", "[unary-ops]") {
 
-    int num_rows = 11;
-    int num_cols = 5;
+    size_t num_rows = 11;
+    size_t num_cols = 5;
     Matrix m1 = Matrix::Random(num_rows, num_cols);
     Matrix m2 = m1.Transpose();
 
-    for (int i = 0; i < num_rows; i++)
-    {
-        for (int j = 0; j < num_cols; j++) {
-            REQUIRE(m2(j, i) == m1(i, j));
+    bool result = true;
+
+    for (size_t i = 0; i < num_rows; i++) {
+        for (size_t j = 0; j < num_cols; j++) {
+            if (m2(j, i) != m1(i, j)) result = false;
+            if (!result) break;
         }
+        if (!result) break;
     }
+
+    REQUIRE(result == true);
 }
