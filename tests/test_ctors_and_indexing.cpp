fd138e289354c7c00369338671ad6e90c1bcf28f
isaiah <iditmer@alaska.edu>
2021-04-16 12:15:03 -0800

    Clean up initializer list tests

--- tests/test_ctors_and_indexing.cpp
+++ tests/test_ctors_and_indexing.cpp
@@ -13,31 +13,37 @@ TEST_CASE("Standard constructor generates correct dimensions", "[ctors]") {
 }
 
 TEST_CASE("Initializer list constructor populates matrix correctly", "[ctors]") {
-
-    NumericVector v1{ 15,16,17 };
-    NumericVector v2{ 22,23,24 };
-    NumericVector v3{ 19,15,11 };
-    NumericVector v4{ 37,30,23 };
-    Matrix m{ v1,v2,v3,v4 };
+    
+    // using a vector of 'vectors' for ease of indexed testing
+    std::vector<NumericVector> rows;
+    rows.push_back(NumericVector{ 15,16,17 });
+    rows.push_back(NumericVector{ 22,23,24 });
+    rows.push_back(NumericVector{ 19,15,11 });
+    rows.push_back(NumericVector{ 37,30,23 });    
+    Matrix m{ rows[0],rows[1],rows[2],rows[3] };
 
     REQUIRE(m.Rows() == 4);
     REQUIRE(m.Columns() == 3);
 
-    REQUIRE(m(0, 0) == 15);
-    REQUIRE(m(0, 1) == 16);
-    REQUIRE(m(0, 2) == 17);
+    bool result = true;
 
-    REQUIRE(m(1, 0) == 22);
-    REQUIRE(m(1, 1) == 23);
-    REQUIRE(m(1, 2) == 24);
+    for (size_t i = 0; i < rows.size(); i++) {
+        for (size_t j = 0; j < 3; j++) {
+            if (m(i, j) != rows[i](j)) result = false;
+            if (!result) break;
+        }
+        if (!result) break;
+    }
 
-    REQUIRE(m(2, 0) == 19);
-    REQUIRE(m(2, 1) == 15);
-    REQUIRE(m(2, 2) == 11);
+    REQUIRE(result == true);
 
-    REQUIRE(m(3, 0) == 37);
-    REQUIRE(m(3, 1) == 30);
-    REQUIRE(m(3, 2) == 23);
+    Matrix m2{
+        NumericVector{ 15,16,17 },NumericVector{ 22,23,24 },
+        NumericVector{ 19,15,11 },NumericVector{ 37,30,23 }
+    };
+
+    result = m == m2;
+    REQUIRE(result == true);
 }
 
 TEST_CASE("Indexing by row and column returns correct value", "[indexing]") {
